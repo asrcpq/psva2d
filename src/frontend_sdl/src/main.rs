f3d237401cc327caeae2d66c94064d59a8c5ab3c
asrcpq <asrcpq@outlook.com>
2022-02-13 09:12:44 +0000

    add frontend reconnect

--- src/frontend_sdl/src/main.rs
+++ src/frontend_sdl/src/main.rs
@@ -5,24 +5,15 @@ use sdl2::keyboard::Keycode;
 
 use std::time::Duration;
 use std::thread::sleep;
-use std::os::unix::net::UnixStream;
 use std::io::Read;
 
-use protocol::Message::{self, *};
+use protocol::Message;
+use protocol::sock::SockClient;
 
 pub fn main() {
-	let mut stream = loop {
-		match UnixStream::connect("psva2d.socket") {
-			Ok(s) => break s,
-			Err(_) => {
-				eprintln!("Waiting connection");
-				sleep(Duration::from_secs(1));
-			},
-		}
-	};
+	let mut sock = SockClient::default();
 	eprintln!("Connection ok");
 	let mut buf = vec![0u8; 10_000_000];
-	stream.set_nonblocking(true).unwrap();
 	let sdl_context = sdl2::init().unwrap();
 	let video_subsystem = sdl_context.video().unwrap();
 	let window = video_subsystem.window("psva2d", 800, 600)
@@ -34,9 +25,7 @@ pub fn main() {
 	canvas.clear();
 	canvas.present();
 	let mut event_pump = sdl_context.event_pump().unwrap();
-	let mut i = 0;
 	'running: loop {
-		i = (i + 1) % 255;
 		for event in event_pump.poll_iter() {
 			match event {
 				Event::Quit {..} |
@@ -47,29 +36,28 @@ pub fn main() {
 			}
 		}
 		canvas.present();
-		let buflen = loop {
-			if let Ok(buflen) = stream.read(&mut buf) {
-				break buflen
+		loop {
+			let msg = sock.read_msg();
+			match msg {
+				// todo: update last only
+				Message::WorldUpdate(pvec) => {
+					eprintln!("Update");
+					canvas.set_draw_color(Color::RGB(0, 0, 0));
+					canvas.clear();
+					for [x, y] in pvec.into_iter() {
+						// overflow is okay
+						canvas.filled_circle(
+							x as i16,
+							y as i16,
+							5,
+							Color::RGB(0, 255, 0),
+						).unwrap();
+					}
+				},
+				Message::Nop => break,
+				_ => {},
 			}
-			sleep(Duration::from_millis(10));
-		};
-		let msg = Message::from_bytes(&buf[..buflen]);
-		match msg {
-			WorldUpdate(pvec) => {
-				eprintln!("Update");
-				canvas.set_draw_color(Color::RGB(0, 0, 0));
-				canvas.clear();
-				for [x, y] in pvec.into_iter() {
-					// overflow is okay
-					canvas.filled_circle(
-						x as i16,
-						y as i16,
-						5,
-						Color::RGB(0, 255, 0),
-					).unwrap();
-				}
-			},
-			_ => {},
 		}
+		std::thread::sleep(Duration::from_millis(10));
 	}
 }
