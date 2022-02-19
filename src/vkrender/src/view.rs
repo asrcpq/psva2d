44e95da2fe15c9afde05e289ca027c2f4de0284e
asrcpq <asrcpq@outlook.com>
2022-02-19 01:38:44 +0000

    keyboard view

--- src/vkrender/src/view.rs
+++ src/vkrender/src/view.rs
@@ -4,22 +4,46 @@ pub struct View {
 	world_center: [f32; 2],
 	screen_center: [f32; 2],
 	scaler: [f32; 2], // consider flip
+	sscaler: f32,
+	scaler0: [f32; 2],
+
+	move_distance: f32,
 }
 
 impl Default for View {
 	fn default() -> Self {
+		let scaler0 = [0.1, -0.16];
 		Self {
 			world_center: [0., 0.],
 			screen_center: [0., 0.],
-			scaler: [0.1, -0.16],
+			scaler: scaler0.clone(),
+			sscaler: 1.0,
+			scaler0,
+
+			move_distance: 0.1,
 		}
 	}
 }
 
 impl View {
-	pub fn move_view(&mut self, ds: [f32; 2]) {
-		self.world_center[0] += ds[0];
-		self.world_center[1] += ds[1];
+	pub fn move_view(&mut self, direction: u8) { // lurd
+		match direction {
+			0 => self.world_center[0] -= self.move_distance,
+			1 => self.world_center[1] += self.move_distance,
+			2 => self.world_center[0] += self.move_distance,
+			3 => self.world_center[1] -= self.move_distance,
+			_ => { eprintln!("ERROR: wrong direction {}", direction) }
+		}
+	}
+
+	pub fn scale_view(&mut self, zoom_in: bool) {
+		if zoom_in {
+			self.sscaler *= 1.5;
+		} else {
+			self.sscaler /= 1.5;
+		}
+		self.scaler[0] = self.scaler0[0] * self.sscaler;
+		self.scaler[1] = self.scaler0[1] * self.sscaler;
 	}
 
 	pub fn w2s(&self, p: [f32; 2]) -> [f32; 2] {
